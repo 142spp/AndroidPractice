f85400732d3adf620688348c052507d84aeb4b66
142spp <ldg142s@gmail.com>
2022-05-03 13:20:37 +0900

    Week7

---
+++ Week6_1/app/src/main/java/com/example/week6_t1/SubActivity.kt
@@ -0,0 +1,13 @@
+package com.example.week6_t1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+    }
+}
